c41ccd37d721500efd21182dd65af4e0b05e2488
arunsridher <45627741+arunsridher@users.noreply.github.com>
2020-02-28 18:24:26 +0530

    add database and complete all functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+$(document).ready(function(){
+    $('#due-date').attr('min', todayDate());
+});
+
 var defaultColor = '#fff';
 var clickedColor = '#f0f0f0';
 
@@ -59,4 +63,24 @@ function deleteContacts(){
         console.log(data);
         location.reload();
     });
+}
+
+function todayDate() {
+    var today = new Date(); // get the current date
+    var dd = today.getDate(); //get the day from today.
+    var mm = today.getMonth()+1; //get the month from today +1 because january is 0!
+    var yyyy = today.getFullYear(); //get the year from today
+
+    //if day is below 10, add a zero before (ex: 9 -> 09)
+    if(dd<10) {
+        dd='0'+dd
+    }
+
+    //like the day, do the same to month (3->03)
+    if(mm<10) {
+        mm='0'+mm
+    }
+
+    //finally join yyyy mm and dd with a "-" between then
+    return yyyy+'-'+mm+'-'+dd;
 }
--- index.js
+++ index.js
@@ -2,75 +2,58 @@ const express = require('express');
 const port = 8000;
 const app = express();
 
+const db = require('./config/mongoose');
+const Card = require('./models/card');
+
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.urlencoded());
 app.use(express.static('assets'));
 
 app.get('/', function(req, res){
-    return res.render('home', {
-        title : "To-Do App",
-        todos : todoList
+    Card.find({}, function(err, todoList){
+        if(err){
+            console.log(`Unable to fetch the cards: ${err}`);
+            return;
+        }
+
+        return res.render('home',{
+            title: "ToDo's",
+            todos : todoList
+        });
     });
 });
 
-var id = 3;
-var todoList = [
-    {
-        title: "Why not add a task?",
-        date: "may 2, 2019",
-        category: "Other",
-        id: 1
-    },
-    {
-        title: "Let's make a todo app",
-        date: "APR 28, 2019",
-        category: "Scool",
-        id: 2
-    },
-    {
-        title: "Annual report submission deadline",
-        date: "Jun 1, 2019",
-        category: "work",
-        id: 3
-    }
-];
-
 app.post("/create-card", function(req, res){
-    todoList.push({
+    let date = new Date(req.body["due-date"]);
+    date = date.toDateString();
+    let arr = date.split(" ");
+    date = arr[1] + " " + arr[2] + ", " + arr[3];
+    Card.create({
         title: req.body.description,
-        date: req.body["due-date"],
+        date: date,
         category: req.body.category
-    });
-    res.redirect('back');
+    }, function(err, newCard){
+        if(err){
+            console.log('eroor in creating a card!');
+            return;
+        }
+        console.log("card inserted");
+        return res.redirect('back');
+    }); 
 });
 
 app.post("/delete", function(req, res){
     let ids = req.body.ids;
     ids.forEach(element => {
-        let cardIndex = todoList.findIndex(card => card.id == element);
-        console.log("cardIndex " + cardIndex);
-        if(cardIndex != -1){
-            todoList.splice(cardIndex, 1);
-        }
+        Card.findByIdAndDelete(element, function(err){
+            if(err){
+                console.log(`unable to delete the card: ${err}`)
+            }
+        });
     });
 
     res.redirect('back');
-    // todoList.findByIdAndDelete(id, function(err){
-    //     if(err){
-    //         console.log("Couldn't delete contact");
-    //         return;    
-    //     }
-
-    //     return res.redirect('back');
-    // });
-    // let contactIndex = contactList.findIndex(contact => contact.phone == phone);
-    
-    // if(contactIndex != -1){
-    //     contactList.splice(contactIndex, 1);
-    // }
-
-    // return res.redirect("back");
 });
 
 app.listen(port, function(err){
